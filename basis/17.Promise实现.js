9558bbfa73610052467cc147d2ccbd6bf62b10a3
yangweixin1406 <327621797@qq.com>
2020-05-14 19:25:38 +0800

    promise添加race和all方法

--- basis/17.Promise实现.js
+++ basis/17.Promise实现.js
@@ -201,6 +201,49 @@ class MyPromise {
   static reject (err) {
     return new MyPromise((resolve, reject) => reject(err))
   }
+
+  // 添加静态all方法
+  static all (list) {
+    return new MyPromise((resolve, reject) => {
+      /**
+       * 返回值的集合
+       */
+      let values = []
+      let count = 0
+      for (let [i, p] of list.entries()) {
+        // 数组参数如果不是MyPromise实例，先调用MyPromise.resolve
+        this.resolve(p).then(res => {
+          values[i] = res
+          count++
+          // 所有状态都变成fulfilled时返回的MyPromise状态就变成fulfilled
+          if (count === list.length) resolve(values)
+        }, err => {
+          // 有一个被rejected时返回的MyPromise状态就变成rejected
+          reject(err)
+        })
+      }
+    })
+  }
+  // 添加静态race方法
+  static race (list) {
+    return new MyPromise((resolve, reject) => {
+      for (let p of list) {
+        // 只要有一个实例率先改变状态，新的MyPromise的状态就跟着改变
+        this.resolve(p).then(res => {
+          resolve(res)
+        }, err => {
+          reject(err)
+        })
+      }
+    })
+  }
+  
+  finally (cb) {
+    return this.then(
+      value  => MyPromise.resolve(cb()).then(() => value),
+      reason => MyPromise.resolve(cb()).then(() => { throw reason })
+    );
+  }
 }
 new MyPromise((resolve, reject) => {
   console.log(13333444445)
