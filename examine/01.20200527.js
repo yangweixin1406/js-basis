7d1f6e406d001a5530cf686d2ad5c11b4a63af0d
yangweixin1406 <327621797@qq.com>
2020-05-28 09:36:30 +0800

    测试题

---
+++ examine/01.20200527.js
@@ -0,0 +1,8 @@
+function getInfo (member, year) {
+  member.name = 'lyida'
+  year = '1998' 
+}
+const member = { name: 'Jaclk' }
+const birthday = '1997'
+getInfo(member, birthday)
+console.log(member, birthday)
